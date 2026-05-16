d94ae6d58eb13c339b4c0f21df755d24325a031c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-16 10:42:51 +0200

    Lay out domain

---
+++ ToDoListSolution/ToDoListBL/Domain/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListBL.Domain
+{
+    public class Todo
+    {
+        public Todo(int id, string title, string description, bool isCompleted)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsCompleted = isCompleted;
+        }
+
+        public int Id {  get; init; }
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public bool IsCompleted { get; init; }
+    }
+}
---
+++ ToDoListSolution/ToDoListBL/Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListBL.Domain
+{
+    public class User
+    {
+        public User(int id, string firstName, string lastName, DateTime birthDate, string pictureURL)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            PictureURL = pictureURL;
+        }
+        public int Id { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public DateTime BirthDate { get; init; }
+        public string PictureURL { get; init; }
+    }
+}
