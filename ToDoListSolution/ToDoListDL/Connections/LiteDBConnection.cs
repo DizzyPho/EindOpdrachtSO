a194f6e76df0ba33488a5426533e7ea8afdf652c
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-16 12:26:20 +0200

    Add connection class to inject

---
+++ ToDoListSolution/ToDoListDL/Connections/LiteDBConnection.cs
@@ -0,0 +1,23 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListDL.Connections
+{
+    public class LiteDBConnection
+    {
+        private string _connectionString = "todolist.db";
+        private LiteDatabase _db;
+
+        public LiteDBConnection()
+        {
+            _db = new LiteDatabase(_connectionString);
+        }
+
+        public ILiteCollection<T> GetCollection<T>()
+        {
+            return _db.GetCollection<T>();
+        }
+    }
+}
