afa28a7a5b5924d54adbc242ba42154726b4106e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-16 11:59:14 +0200

    Add user id property

--- ToDoListSolution/ToDoListBL/Domain/Todo.cs
+++ ToDoListSolution/ToDoListBL/Domain/Todo.cs
@@ -6,17 +6,19 @@ namespace ToDoListBL.Domain
 {
     public class Todo
     {
-        public Todo(int id, string title, string description, bool isCompleted)
+        public Todo(int id, string title, string description, bool isCompleted, int responsibleUserId)
         {
             Id = id;
             Title = title;
             Description = description;
             IsCompleted = isCompleted;
+            ResponsibleUserId = responsibleUserId;
         }
 
         public int Id {  get; init; }
         public string Title { get; init; }
         public string Description { get; init; }
         public bool IsCompleted { get; init; }
+        public int ResponsibleUserId { get; init; }
     }
 }
