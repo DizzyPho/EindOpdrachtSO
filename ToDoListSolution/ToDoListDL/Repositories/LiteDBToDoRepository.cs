9c267de2e8b10c1b40c5e9d441c36cefc31bfbdc
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-16 12:10:33 +0200

    Add repository

---
+++ ToDoListSolution/ToDoListDL/Repositories/LiteDBToDoRepository.cs
@@ -0,0 +1,18 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Interfaces;
+
+namespace ToDoListDL.Repositories
+{
+    public class LiteDBToDoRepository : IToDoRepository
+    {
+        public LiteDatabase _db;
+
+        public LiteDBToDoRepository(LiteDatabase db)
+        {
+            _db = db;
+        }
+    }
+}
