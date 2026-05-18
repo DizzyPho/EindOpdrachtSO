2ae3c6615fbf669d40fc303e83e75bdc98c9962f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 11:45:50 +0200

    Add navigation service

---
+++ ToDoListSolution/ToDoListGUI/Services/NavigationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListGUI.Services
+{
+    public class NavigationService
+    {
+        private Shell _shell;
+
+        public NavigationService()
+        {
+            _shell = Shell.Current;
+        }
+
+        public async Task Navigate(string route, Dictionary<string, object> parameters)
+        {
+            await _shell.GoToAsync(route, parameters);
+        }
+        public async Task GoBack(Dictionary<string, object> parameters)
+        {
+            await Navigate("..", parameters);
+        }
+    }
+}
