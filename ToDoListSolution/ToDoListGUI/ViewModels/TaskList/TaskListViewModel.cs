5bc3115a3a3ac2ab27671351d86a9d899fc12db0
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 12:45:25 +0200

    Navigate to window

--- ToDoListSolution/ToDoListGUI/ViewModels/TaskList/TaskListViewModel.cs
+++ ToDoListSolution/ToDoListGUI/ViewModels/TaskList/TaskListViewModel.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 using ToDoListBL.Services;
+using ToDoListGUI.Routes;
+using ToDoListGUI.Services;
 
 namespace ToDoListGUI.ViewModels.TaskList
 {
     public class TaskListViewModel
     {
-        public TaskListViewModel(ToDoService toDoService)
+        NavigationService _navigation;
+        public TaskListViewModel(ToDoService toDoService, NavigationService navigationService)
         {
             Tasks = new ObservableCollection<TaskViewModel>(
                                             toDoService.GetTasks()
                                             .Select(todo => new TaskViewModel(todo, toDoService)));
+            _navigation = navigationService;
+
+            NewTaskCommand = new Command(OnNewTask);
+            
         }
 
         public ObservableCollection<TaskViewModel> Tasks { get; }
+        public ICommand NewTaskCommand { get; init; }
+
+        public void OnNewTask()
+        {
+            // task.run causes COMexception
+            _navigation.Navigate(RouteDefinitions.TaskDetailRoute);
+        }
     }
 }
