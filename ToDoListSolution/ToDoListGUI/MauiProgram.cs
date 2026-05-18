3ef26816322879a9266b2044acbc44c8126ca61d
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 11:42:43 +0200

    Register routes

--- ToDoListSolution/ToDoListGUI/MauiProgram.cs
+++ ToDoListSolution/ToDoListGUI/MauiProgram.cs
@@ -1,9 +1,11 @@
 ﻿using LiteDB;
 using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
 using ToDoListBL.Interfaces;
 using ToDoListBL.Services;
 using ToDoListDL.Connections;
 using ToDoListDL.Repositories;
+using ToDoListGUI.Routes;
 
 namespace ToDoListGUI
 {
@@ -20,6 +22,7 @@ namespace ToDoListGUI
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 })
                 .RegisterServices()
+                .RegisterRoutes()
                 ;
 
 #if DEBUG
@@ -29,11 +32,21 @@ namespace ToDoListGUI
             return builder.Build();
         }
 
-        public static void RegisterServices(this MauiAppBuilder builder)
+        public static MauiAppBuilder RegisterServices(this MauiAppBuilder builder)
         {
             builder.Services.AddSingleton<LiteDBConnection>();
             builder.Services.AddTransient<IToDoRepository, LiteDBToDoRepository>();
             builder.Services.AddTransient<ToDoService>();
+            return builder;
+        }
+
+        public static MauiAppBuilder RegisterRoutes(this MauiAppBuilder builder)
+        {
+            Routing.RegisterRoute(RouteDefinitions.TaskListRoute, RouteDefinitions.TaskListType);
+            Routing.RegisterRoute(RouteDefinitions.TaskDetailRoute, RouteDefinitions.TaskDetailType);
+            Routing.RegisterRoute(RouteDefinitions.UserListRoute, RouteDefinitions.UserListType);
+            Routing.RegisterRoute(RouteDefinitions.UserDetailRoute, RouteDefinitions.UserDetailType);
+            return builder;
         }
     }
 }
