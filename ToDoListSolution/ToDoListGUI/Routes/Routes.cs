1e5ab81eff65cb36539e24d5a7c9a76a1db3f6ef
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 11:30:34 +0200

    Define routes and types

---
+++ ToDoListSolution/ToDoListGUI/Routes/Routes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListGUI.Pages;
+
+namespace ToDoListGUI.Routes
+{
+    public static class Routes
+    {
+        public const string TaskDetailRoute = "TaskDetailPage";
+        public const string TaskListRoute= "TaskListPage";
+        public const string UserDetailRoute = "UserDetailPage";
+        public const string UserListRoute = "UserListPage";
+
+        public static Type TaskDetailType = typeof(TaskDetailPage);
+        public static Type TaskListType = typeof(TaskListPage);
+        public static Type UserDetailType = typeof(UserDetailPage);
+        public static Type UserListType = typeof(UserDetailPage);
+    }
+}
