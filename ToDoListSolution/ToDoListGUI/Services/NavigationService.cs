958e5ed19572ad90a6e62ffb286b209027d2916a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 12:15:23 +0200

    Set parameters null by default

--- ToDoListSolution/ToDoListGUI/Services/NavigationService.cs
+++ ToDoListSolution/ToDoListGUI/Services/NavigationService.cs
@@ -6,11 +6,11 @@ namespace ToDoListGUI.Services
 {
     public class NavigationService
     {
-        public async Task Navigate(string route, Dictionary<string, object>? parameters)
+        public async Task Navigate(string route, Dictionary<string, object>? parameters = null)
         {
             await Shell.Current.GoToAsync(route, parameters);
         }
-        public async Task GoBack(Dictionary<string, object> parameters)
+        public async Task GoBack(Dictionary<string, object>? parameters = null)
         {
             await Navigate("..", parameters);
         }
