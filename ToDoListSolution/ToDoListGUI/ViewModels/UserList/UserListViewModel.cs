e01255ef088a09a1f868b655718e927089fd3ad9
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 14:10:19 +0200

    Show all users

--- ToDoListSolution/ToDoListGUI/ViewModels/UserList/UserListViewModel.cs
+++ ToDoListSolution/ToDoListGUI/ViewModels/UserList/UserListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using ToDoListBL.Services;
 using ToDoListGUI.Services;
@@ -10,10 +11,14 @@ namespace ToDoListGUI.ViewModels.UserList
     {
         ToDoService _toDoService;
         NavigationService _navigation;
+
+        public ObservableCollection<UserViewModel> Users { get; init; }
         public UserListViewModel(ToDoService toDoService, NavigationService navigationService) 
         {
             _toDoService = toDoService;
             _navigation = navigationService;
+            var userList = _toDoService.GetUsers();
+            Users = new ObservableCollection<UserViewModel>(userList.Select(user => new UserViewModel(user)));
         }
     }
 }
---
+++ ToDoListSolution/ToDoListGUI/ViewModels/UserList/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Domain;
+
+namespace ToDoListGUI.ViewModels.UserList
+{
+    public class UserViewModel : BaseViewModel
+    {
+        public UserViewModel(User user)
+        {
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            // possible to make age int and format 
+            var diff = (DateTime.Now - user.BirthDate);
+            Age = user.GetAge();
+        }
+
+        public string FirstName
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public string LastName
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public int Age
+        {
+            get => Get<int>();
+            set => Set(value);
+        }
+    }
+}
