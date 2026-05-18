462ba4d6ddfb4935cd31d82e5356f55e2c561462
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 14:34:34 +0200

    Add binding context to page

--- ToDoListSolution/ToDoListGUI/Pages/UserDetailPage.xaml.cs
+++ ToDoListSolution/ToDoListGUI/Pages/UserDetailPage.xaml.cs
@@ -1,9 +1,24 @@
+using ToDoListGUI.ViewModels;
+using ToDoListGUI.ViewModels.UserDetail;
+
 namespace ToDoListGUI.Pages;
 
-public partial class UserDetailPage : ContentPage
+public partial class UserDetailPage : ContentPage, IQueryAttributable
 {
 	public UserDetailPage()
 	{
         InitializeComponent();
+    }
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if(query.TryGetValue("id", out object oId) && oId is string id)
+        {
+            BindingContext = new UserDetailViewModel(id);
+        }
+        else
+        {
+            BindingContext = new UserDetailViewModel();
+        }
     }
 }
---
+++ ToDoListSolution/ToDoListGUI/ViewModels/UserDetail/UserDetailViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Services;
+using ToDoListGUI.Services;
+
+namespace ToDoListGUI.ViewModels.UserDetail
+{
+    public class UserDetailViewModel : BaseViewModel
+    {
+        NavigationService _navigation;
+        ToDoService _toDoService;
+        public string FirstName 
+        {
+            get => Get<String>(); 
+            set => Set(value); 
+        }
+        public string LastName
+        {
+            get => Get<String>();
+            set => Set(value);
+        }
+        public string ImageUrl
+        {
+            get => Get<String>();
+            set => Set(value);
+        }
+        public DateTime DateOfBirth
+        {
+            get => Get<DateTime>();
+            set => Set(value);
+        }
+        public UserDetailViewModel(string id, ToDoService toDoService, NavigationService navigationService) 
+            : this(toDoService, navigationService)
+        {
+        }
+
+        public UserDetailViewModel(ToDoService toDoService, NavigationService navigationService)
+        {
+            _toDoService = toDoService;
+            _navigation = navigationService;
+        }
+    }
+}
