4eb085141a77e60e4dd8c33fb0064a718b06a93d
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-18 13:51:06 +0200

    Attach binding context to page

--- ToDoListSolution/ToDoListGUI/Pages/UserListPage.xaml.cs
+++ ToDoListSolution/ToDoListGUI/Pages/UserListPage.xaml.cs
@@ -1,9 +1,14 @@
+using ToDoListBL.Services;
+using ToDoListGUI.Services;
+using ToDoListGUI.ViewModels.UserList;
+
 namespace ToDoListGUI.Pages;
 
 public partial class UserListPage : ContentPage
 {
-	public UserListPage()
+	public UserListPage(ToDoService toDoService, NavigationService navigationService)
 	{
 		InitializeComponent();
+		BindingContext = new UserListViewModel(toDoService, navigationService);
 	}
 }
---
+++ ToDoListSolution/ToDoListGUI/ViewModels/UserList/UserListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Services;
+using ToDoListGUI.Services;
+
+namespace ToDoListGUI.ViewModels.UserList
+{
+    public class UserListViewModel
+    {
+        ToDoService _toDoService;
+        NavigationService _navigation;
+        public UserListViewModel(ToDoService toDoService, NavigationService navigationService) 
+        {
+            _toDoService = toDoService;
+            _navigation = navigationService;
+        }
+    }
+}
