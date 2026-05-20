c58a9a8312a47c05e823703df83391facfc0506e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-20 09:59:48 +0200

    Add new message

---
+++ ToDoListSolution/ToDoListBL/Messages/UserUpdatedMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Domain;
+
+namespace ToDoListBL.Messages
+{
+    public class UserUpdatedMessage
+    {
+        public UserUpdatedMessage(User user)
+        {
+            User = user;
+        }
+
+        public User User { get; init; }
+    }
+}
