2bb760c683b412772bf6ce60d522d67ff35196dd
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-20 09:59:19 +0200

    Add async command

---
+++ ToDoListSolution/ToDoListGUI/Commands/AsyncCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListGUI.Commands
+{
+    public partial class AsyncCommand : Command
+    {
+        public AsyncCommand(Func<Task> action) : base(async () => await action())
+        {
+            
+        }
+    }
+}
