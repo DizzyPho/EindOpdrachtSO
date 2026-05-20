fe2e3955a966de6929b6e81680514e997fa9084e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-20 09:17:00 +0200

    Create message service

---
+++ ToDoListSolution/ToDoListBL/Services/MessageService.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListBL.Services
+{
+    public class MessageService
+    {
+        public void Send<T>(T message)
+            where T : class
+        {
+            WeakReferenceMessenger.Default.Send<T>(message);
+        }
+
+        public void Register<T>(object recipient, MessageHandler<object, T> handler)
+            where T : class
+        {
+            WeakReferenceMessenger.Default.Register<T>(recipient, handler);
+        }
+    }
+}
