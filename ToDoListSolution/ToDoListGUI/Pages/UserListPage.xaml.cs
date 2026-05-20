36c8ac0fa8cd86357d797b6331cf54f4e2831084
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-20 10:06:58 +0200

    Inject service

--- ToDoListSolution/ToDoListGUI/Pages/UserListPage.xaml.cs
+++ ToDoListSolution/ToDoListGUI/Pages/UserListPage.xaml.cs
@@ -6,9 +6,9 @@ namespace ToDoListGUI.Pages;
 
 public partial class UserListPage : ContentPage
 {
-	public UserListPage(ToDoService toDoService, NavigationService navigationService)
+	public UserListPage(ToDoService toDoService, NavigationService navigationService, MessageService messageService)
 	{
 		InitializeComponent();
-		BindingContext = new UserListViewModel(toDoService, navigationService);
+		BindingContext = new UserListViewModel(toDoService, navigationService, messageService);
 	}
 }
