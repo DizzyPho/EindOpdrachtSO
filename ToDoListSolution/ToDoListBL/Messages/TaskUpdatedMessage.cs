a935d1a2a7020d220b81a148df786effd1e55b76
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-20 20:18:49 +0200

    Create new message

---
+++ ToDoListSolution/ToDoListBL/Messages/TaskUpdatedMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Domain;
+
+namespace ToDoListBL.Messages
+{
+    public class TaskUpdatedMessage
+    {
+        public TaskUpdatedMessage(Todo updatedTask)
+        {
+            UpdatedTask = updatedTask;
+        }
+
+        public Todo UpdatedTask { get; init; }
+    }
+}
