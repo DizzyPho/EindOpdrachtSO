1205026501ce51b4877c41d6d8af3c696c537338
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-21 11:34:10 +0200

    Add filter strategies

---
+++ ToDoListSolution/ToDoListGUI/Strategies/TaskFiltering/IFilterStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ToDoListBL.Domain;
+
+namespace ToDoListGUI.Strategies.TaskFiltering
+{
+    public interface IFilterStrategy
+    {
+        public List<Todo> Filter(List<Todo> tasks);
+    }
+}
---
+++ ToDoListSolution/ToDoListGUI/Strategies/TaskFiltering/UnfinishedFilterStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Domain;
+
+namespace ToDoListGUI.Strategies.TaskFiltering
+{
+    public class UnfinishedFilterStrategy : IFilterStrategy
+    {
+        public List<Todo> Filter(List<Todo> tasks)
+        {
+            return tasks.Where(task => task.IsCompleted == false).ToList();
+        }
+    }
+}
