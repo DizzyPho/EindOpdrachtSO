264fa1afbe8e99972d9ac2939b90a9d3125c66b5
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-21 11:39:21 +0200

    Add sorting strategy

---
+++ ToDoListSolution/ToDoListGUI/Strategies/TaskSorting/RecencySortStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListBL.Domain;
+
+namespace ToDoListGUI.Strategies.TaskSorting
+{
+    internal class RecencySortStrategy : IComparer<Todo>
+    {
+        public int Compare(Todo? x, Todo? y)
+        {
+            return DateTime.Compare(x.CreationDate, y.CreationDate);
+        }
+    }
+}
