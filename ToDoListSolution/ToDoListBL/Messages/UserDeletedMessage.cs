165684f9be67d085b58d3910506946d5fe03651e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-22 12:08:43 +0200

    Add new message

---
+++ ToDoListSolution/ToDoListBL/Messages/UserDeletedMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListBL.Messages
+{
+    public class UserDeletedMessage
+    {
+        public UserDeletedMessage(string userId)
+        {
+            UserId = userId;
+        }
+
+        public string UserId { get; init; }
+    }
+}
