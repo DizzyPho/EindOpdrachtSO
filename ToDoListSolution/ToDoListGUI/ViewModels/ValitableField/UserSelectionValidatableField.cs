14e51237bea6f92212cf740a1a5ddb6bfb8a54ac
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-24 12:23:47 +0200

    Add new type of validatable field

---
+++ ToDoListSolution/ToDoListGUI/ViewModels/ValitableField/UserSelectionValidatableField.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoListGUI.ViewModels.TaskDetail;
+using ToDoListGUI.ViewModels.UserList;
+
+namespace ToDoListGUI.ViewModels.ValitableField
+{
+    public class NotNullValidatableField<T> : AbstractValidatableField<T>
+        where T : class
+    {
+        public NotNullValidatableField(Brush validColor, Brush invalidColor) : base(validColor, invalidColor)
+        {
+        }
+
+        public override bool Validate()
+        {
+            IsValid = Value != null;
+            return base.Validate();
+        }
+    }
+}
