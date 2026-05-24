a36e60e39bbd69cdd899b0c1ae09132268bd44e6
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-05-24 12:12:42 +0200

    Add validatable fields

---
+++ ToDoListSolution/ToDoListGUI/ViewModels/ValitableField/AbstractValidatableField.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListGUI.ViewModels.ValitableField
+{
+    public abstract class AbstractValidatableField<T> : BaseViewModel
+    {
+        public T Value
+        {
+            get => Get<T>();
+            set => Set(value);
+        }
+        public bool IsValid
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        private Brush _validColor;
+        private Brush _invalidColor;
+
+        protected AbstractValidatableField(Brush validColor, Brush invalidColor)
+        {
+            _validColor = validColor;
+            _invalidColor = invalidColor;
+            CurrentColor = default;
+        }
+
+        public Brush CurrentColor
+        {
+            get => Get<Brush>();
+            set => Set(value);
+        }
+        public virtual bool Validate() 
+        {
+            CurrentColor = IsValid ? _validColor : _invalidColor;
+            return IsValid;
+        }
+    }
+}
---
+++ ToDoListSolution/ToDoListGUI/ViewModels/ValitableField/NonEmptyValidatableField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListGUI.ViewModels.ValitableField
+{
+    public class NonEmptyValidatableField : AbstractValidatableField<string>
+    {
+        public NonEmptyValidatableField(Brush validColor, Brush invalidColor) : base(validColor, invalidColor)
+        {
+        }
+
+        public override bool Validate()
+        {
+            IsValid = !string.IsNullOrWhiteSpace(Value);
+            return base.Validate();
+        }
+    }
+}
